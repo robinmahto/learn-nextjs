20419f555f0a40ca23abaaca4f9dae9ff03dfe56
robinmahto <robinmahto413@gmail.com>
2025-02-18 00:15:44 +0530

    `learn about re-useable layouts`

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,24 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Learn Next.js",
-  description: "we are learning how to make next.js application",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <header
+          style={{
+            background: "#3333",
+          }}
+        >
+          Header
+        </header>
         {children}
+        <footer
+          style={{
+            background: "#7777",
+          }}
+        >
+          Footer
+        </footer>
       </body>
     </html>
   );
